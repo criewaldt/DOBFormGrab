e1349cab8430dae710f2e97de389c54fe8a4233e
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2022-11-21 02:53:39 -0500

    run

--- DOBFormGrab.py
+++ DOBFormGrab.py
@@ -1,7 +1,20 @@
 #DOBFormGrab
 from bs4 import BeautifulSoup
+import requests
 
-with open("index.html") as fp:
+
+with open("dobForms.mhtml") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
+links = soup.find_all('a')
+for l in links:
+
+    g = l.string
+    if g is not None:
+
+        if "Form" in g:
+            g = g.replace('/', '-')
+            r = requests.get("https://www.nyc.gov/{}".format(l['href']), allow_redirects=True)
+            with open("Forms/{}.pdf".format(g), 'wb') as pdf:
+                pdf.write(r.content)
+                print('Downloaded: {}'.format(g))
