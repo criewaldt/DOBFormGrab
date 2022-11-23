14992061bcf83c10343ebf130dba130a175836bf
Christopher D. Riewaldt <github+criewaldt@gmail.com>
2022-11-22 23:16:22 -0500

    cleanup

--- DOBFormGrab.py
+++ DOBFormGrab.py
@@ -7,18 +7,22 @@ import os
 FORMS_URL = "https://www.nyc.gov/site/buildings/dob/forms.page"
 OUTPUT_FOLDER = os.path.abspath("Forms")
 
+#create output folder if it doesnt exist
+exists = os.path.exists(OUTPUT_FOLDER)
+if not exists:
+    os.makedirs(OUTPUT_FOLDER)
+print("Forms will be downloaded into: {}".format(OUTPUT_FOLDER))
 
+#helper to download form
 def download_form(name=None, url=None):
     r = requests.get("https://www.nyc.gov/{}".format(url), allow_redirects=True)
-
     with open(os.path.join(OUTPUT_FOLDER ,"{}.pdf".format(name)), 'wb') as pdf:
         pdf.write(r.content)
         print('Downloaded: {}'.format(name))
 
-
+#helper to clean links
 def clean_links(links=None):
     clean_links = []
-
     for l in links:
         name = l.string
         url = l['href']
@@ -28,7 +32,6 @@ def clean_links(links=None):
                 #clean name
                 name = name.replace('/', '-')
                 clean_links.append({'name':name,'url':url})
-
     return clean_links
 
 
